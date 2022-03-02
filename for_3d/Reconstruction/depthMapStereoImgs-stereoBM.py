c7c2d4837d73a145fc7838e03dc9826fe15ffc62
insung3511 <insung.park123@gmail.com>
2022-03-02 19:11:43 +0900

    bug fix

--- for_3d/Reconstruction/depthMapStereoImgs-stereoBM.py
+++ for_3d/Reconstruction/depthMapStereoImgs-stereoBM.py
@@ -2,27 +2,30 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-capL = cv2.VideoCapture(0)
-capR = cv2.VideoCapture(2)
+# capL = cv2.VideoCapture(0)
+# capR = cv2.VideoCapture(2)
+
+capL = cv2.imread('car_left.png')
+capR = cv2.imread('car_right.png')
 
 while True:
-    if capR and capL is False:
+    #if capR and capL is False:
         print("Camera NOT DETECTED")
         break
     
-    else:
-        retL, frameL = capL.read()
-        retR, frameR = capR.read()
+    
+    retL, frameL = capL.read()
+    retR, frameR = capR.read()
 
-        imgL = cv2.cvtColor(frameL, cv2.COLOR_BGR2GRAY)
-        imgR = cv2.cvtColor(frameR, cv2.COLOR_BGR2GRAY)
+    imgL = cv2.cvtColor(frameL, cv2.COLOR_BGR2GRAY)
+    imgR = cv2.cvtColor(frameR, cv2.COLOR_BGR2GRAY)
 
-        stereo = cv2.StereoBM_create()
-        disparity = stereo.compute(imgL, imgR)
+    stereo = cv2.StereoBM_create()
+    disparity = stereo.compute(imgL, imgR)
         
-        cv2.imshow('gray', disparity)
-        if cv2.waitKey(1) == ord('q'):
-            break
+    cv2.imshow('gray', disparity)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
 capL.release()
 capR.release()
